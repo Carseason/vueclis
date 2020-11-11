ef129b1384928b99751e96f61b0a56d56249a12d
carseason <757841203@qq.com>
2020-11-11 19:05:06 +0800

    i axios

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 import Vue from 'vue';
 Vue.component('icon', () => import("./icon/icon.vue"))          //图标
-Vue.component('tooltip', () => import("./tooltip/tooltip.vue"))
-Vue.component('dialog', () => import("./dialog/dialog.vue")) 
+Vue.component('v-tooltip', () => import("./tooltip/tooltip.vue"))
+Vue.component('v-dialog', () => import("./dialog/dialog.vue")) 
